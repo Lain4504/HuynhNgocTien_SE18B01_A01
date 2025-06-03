deebf7857c3e355c55a6d62cb7cd69f324a09663
Lain <134944634+Lain4504@users.noreply.github.com>
2025-06-03 07:33:17 +0700

    create project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QE180039_PRN222_A01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
