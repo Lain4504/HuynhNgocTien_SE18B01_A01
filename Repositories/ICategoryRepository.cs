eb9eac0cdfd89d323ecb4cfc347247660296120a
Lain <134944634+Lain4504@users.noreply.github.com>
2025-06-03 08:05:20 +0700

    refactoring code

--- Repositories/ICategoryRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using HuynhNgocTien_SE18B01_A01.Models;
-
-namespace HuynhNgocTien_SE18B01_A01.Repositories
-{
-    public interface ICategoryRepository
-    {
-        List<Category> GetCategories();
-    }
-}
---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace HuynhNgocTien_SE18B01_A01.ViewModel
+
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+    }
+
+}
