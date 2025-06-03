5e43a71447223845222bdf21495a5677e987cc39
Lain <134944634+Lain4504@users.noreply.github.com>
2025-06-03 15:25:00 +0700

    New structure

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HuynhNgocTien_SE18B01_A01.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
